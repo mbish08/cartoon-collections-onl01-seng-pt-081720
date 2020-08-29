b0bb21f1dbc7bf8942fa26c274483be407de9950
Mikel Bishop <mdrew8@gmail.com>
2020-08-29 04:34:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def long_planeteer_calls(words)
 end
 
 def find_the_cheese(cheese_types)
-  i = 0 
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
-  return cheese_types[i] if array.include?(cheese_types[i])
-  i += 1 
-end 
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
